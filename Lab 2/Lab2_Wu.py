7d2a2ef63c3586e27beb84b004861db88cc7d848
Student <Student@et.qcc.edu>
2026-01-29 12:59:58 -0500

    Add lab2 Python file

--- Lab 2/Lab2_Wu.py
+++ Lab 2/Lab2_Wu.py
@@ -4,3 +4,9 @@ Lab 2: Mastering Git workflow
 """
 username = input("Enter a username: ")
 print(f"Welcome to Git commands {username}")
+
+# testing 2: calulate the hypotenuse 
+x = 5
+y = 3
+hyp = (x**2 + y**2)**0.5
+print(f"The hypotenuse of sides {x} and {y} is {hyp:0.2f}")
